af94fd7f26db2e0f3da06555589a04a7e02b57ad
dheeraj-singh03 <itsdheeru.singh@gmail.com>
2022-03-24 19:56:19 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
-  source  = "app.terraform.io/my-organisation-dheeraj/s3-bucket/aws"
+  source  = "app.terraform.io/Gaurav/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
 }
